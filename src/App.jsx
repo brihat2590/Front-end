16391accb1b3bd8a2d1e1f03653d0b90c708a030
Brihat576 <thapabrihatbir321@gmail.com>
2024-11-15 12:44:57 +0530

    created feature section below

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import HeroSection from './Components/HeroSection'
+import FeatureSection from './Components/FeatureSection'
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <div className='max-w-7xl mx-auto pt-20 px-6'></div>
       <HeroSection/>
+      <FeatureSection/>
     </div>
   )
 }
