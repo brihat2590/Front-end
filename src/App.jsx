6b96a11a44650227e2a039328ee39c47de0cf370
Brihat576 <thapabrihatbir321@gmail.com>
2024-11-16 16:19:58 +0530

    added the workflow section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Components/Navbar'
 import HeroSection from './Components/HeroSection'
 import FeatureSection from './Components/FeatureSection'
+import WorkFlow from './Components/WorkFlow'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <div className='max-w-7xl mx-auto pt-20 px-6'></div>
       <HeroSection/>
       <FeatureSection/>
+      <WorkFlow/>
     </div>
   )
 }
