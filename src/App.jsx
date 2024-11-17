f5fc3b7b05444a10fec2aa60d261e1696d9b77fa
Brihat576 <thapabrihatbir321@gmail.com>
2024-11-17 17:14:07 +0530

    added the pricing section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar'
 import HeroSection from './Components/HeroSection'
 import FeatureSection from './Components/FeatureSection'
 import WorkFlow from './Components/WorkFlow'
+import Pricing from './Components/Pricing'
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <HeroSection/>
       <FeatureSection/>
       <WorkFlow/>
+      <Pricing/>
     </div>
   )
 }
